e37ba78ae80ca233bd67d23f89414481958c1889
Mason <mvo223@lehigh.edu>
2022-02-11 18:12:53 -0500

    Assignment 2.R

---
+++ Assignment 2.R
@@ -0,0 +1,21 @@
+rm(list=ls(all=TRUE))
+cat("\014")
+library(tidyverse)
+setwd("./covid-19-data/")
+STATES <- read_csv("us-states.csv")
+View(STATES)
+PENNSYLVANIA <- filter(STATES, state=="Pennsylvania")
+View(PENNSYLVANIA)
+PENNSYLVANIA$incr_cases <- 1
+n <- length(PENNSYLVANIA$date)
+for (i in 2:n) {
+  PENNSYLVANIA$incr_cases[i] <- PENNSYLVANIA$cases[i] - PENNSYLVANIA$cases[i-1]
+}
+
+PENNSYLVANIA$incr_deaths <- 1
+for (i in 2:n) {
+  PENNSYLVANIA$incr_deaths[i] <- PENNSYLVANIA$deaths[i] - PENNSYLVANIA$deaths[i-1]
+}
+
+sd <- sd(PENNSYLVANIA$incr_cases)
+cat("The standard deviation of the increment cases in PA is", sd)
