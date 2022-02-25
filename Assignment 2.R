6534e8586de114caf6f49507b713504d71cc03f1
Mason <mvo223@lehigh.edu>
2022-02-25 12:24:49 -0500

    update

--- Assignment 2.R
+++ Assignment 2.R
@@ -6,13 +6,15 @@ STATES <- read_csv("us-states.csv")
 View(STATES)
 PENNSYLVANIA <- filter(STATES, state=="Pennsylvania")
 View(PENNSYLVANIA)
-PENNSYLVANIA$incr_cases <- 1
+PENNSYLVANIA$incr_cases <- 0
+PENNSYLVANIA$incr_cases[1] <- PENNSYLVANIA$cases[1]
 n <- length(PENNSYLVANIA$date)
 for (i in 2:n) {
   PENNSYLVANIA$incr_cases[i] <- PENNSYLVANIA$cases[i] - PENNSYLVANIA$cases[i-1]
 }
 
-PENNSYLVANIA$incr_deaths <- 1
+PENNSYLVANIA$incr_deaths <- 0
+PENNSYLVANIA$incr_deaths[1] <- PENNSYLVANIA$deaths[1]
 for (i in 2:n) {
   PENNSYLVANIA$incr_deaths[i] <- PENNSYLVANIA$deaths[i] - PENNSYLVANIA$deaths[i-1]
 }
